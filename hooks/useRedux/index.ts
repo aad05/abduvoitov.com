2ef5ae1dc04187a98c2d4350234144ee6711521a
aad05 <abduvoitovasadbek3@gmail.com>
2023-10-29 00:19:19 +0500

    redux --remove

--- hooks/useRedux/index.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-import type { RootStore, AppDispatch } from "../../redux";
-
-export const useReduxDispatch = () => useDispatch<AppDispatch>();
-export const useReduxSelector: TypedUseSelectorHook<RootStore> = useSelector;
